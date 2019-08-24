05b349a956226fce0bfaf1913097fc41899d57cb
Misko Kranjec <misko.kranjec@gmail.com>
2019-08-25 00:06:22 +0200

    Fix typo

--- c01/js/add-content.js
+++ c01/js/add-content.js
@@ -2,14 +2,14 @@ let today = new Date();
 let hourNow = today.getHours();
 let greeting;
 
-if (hourNow > 16) {
-  greeting = 'Good evening!';
+if (hourNow > 18) {
+  greeting = "Good evening!";
 } else if (hourNow > 12) {
-  greeting = 'Good afternoon!';
-} else if (hourNow > 0); {
-  greeting = 'Good morning!';
+  greeting = "Good afternoon!";
+} else if (hourNow > 0) {
+  greeting = "Good morning!";
 } else {
-  greeting = 'Welcome!';
+  greeting = "Welcome!";
 }
 
-document.write('<h3>' + greeting + '</h3>');
+document.write("<h3>" + greeting + "</h3>");
