6d4b0ec57e4090ecedbcc3a085242132f8bdafa7
Misko Kranjec <misko.kranjec@gmail.com>
2019-08-26 23:31:43 +0200

    Add comments

--- c01/js/add-content.js
+++ c01/js/add-content.js
@@ -1,7 +1,10 @@
-let today = new Date();
-let hourNow = today.getHours();
+/* This script displays a greeting to the user based upon the current time. */
+
+let today = new Date(); // Create a new date object
+let hourNow = today.getHours(); // Find thr current hour
 let greeting;
 
+// Display the appropriate greeting based on the current time
 if (hourNow > 18) {
   greeting = "Good evening!";
 } else if (hourNow > 12) {
