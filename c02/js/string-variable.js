b6114c2d61ac25b48cd17bdd9c94a46c5d757c00
Misko Kranjec <misko.kranjec@gmail.com>
2019-08-27 23:21:13 +0200

    add replace name in greeting

---
+++ c02/js/string-variable.js
@@ -0,0 +1,9 @@
+let username;
+let message;
+username = "Molly";
+message = "See our upcoming range";
+
+let elName = document.getElementById("name");
+elName.textContent = username;
+let elNote = document.getElementById("note");
+elNote.textContent = message;
