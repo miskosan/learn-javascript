201bbd064b1c3bbbe07df5644b6a17683267bea1
Misko Kranjec <misko.kranjec@gmail.com>
2019-09-07 21:33:59 +0200

    add single value function

---
+++ concept-examples/calculate-area.js
@@ -0,0 +1,9 @@
+// getting a single value out of a function
+
+function calculateArea(width, height) {
+  var area = width * height;
+  return area;
+}
+
+var wallOne = calculateArea(3, 5);
+var wallTwo = calculateArea(8, 5);
