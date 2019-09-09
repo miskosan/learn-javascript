03a3443d4284b3b16fe4beca6b16af04b59f11b0
Misko Kranjec <misko.kranjec@gmail.com>
2019-09-09 23:28:11 +0200

    add comments

--- c03/js/multiple-objects.js
+++ c03/js/multiple-objects.js
@@ -1,3 +1,4 @@
+// Create the template for objects that are hotels
 function Hotel(name, rooms, booked) {
   this.name = name;
   this.rooms = rooms;
@@ -6,9 +7,12 @@ function Hotel(name, rooms, booked) {
     return this.rooms - this.booked;
   };
 }
+
+// Create two hotel objects
 var quayHotel = new Hotel("Quay", 40, 25);
 var parkHotel = new Hotel("Park", 120, 77);
 
+// Update the HTML for the page
 var details1 = quayHotel.name + " rooms: ";
 details1 += quayHotel.checkAvailability();
 var elHotel1 = document.getElementById("hotel1");
