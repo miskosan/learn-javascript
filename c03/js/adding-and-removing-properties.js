35fad4c4ee3eeb0cb7a6c84c5699129bf983d164
Misko Kranjec <misko.kranjec@gmail.com>
2019-09-10 23:21:54 +0200

    add comments

--- c03/js/adding-and-removing-properties.js
+++ c03/js/adding-and-removing-properties.js
@@ -1,18 +1,20 @@
+// Set up the object
 var hotel = {
   name: "Park",
   rooms: 120,
   booked: 77
 };
 
+// Update the HTML
 hotel.gym = true;
 hotel.pool = false;
 delete hotel.booked;
 
-var elName = document.getElementById("hotelName");
-elName.textContent = hotel.name;
+var elName = document.getElementById("hotelName"); // Get element
+elName.textContent = hotel.name; // Update HTML with property of the object
 
-var elPool = document.getElementById("pool");
-elPool.className = "Pool: " + hotel.pool;
+var elPool = document.getElementById("pool"); // Get element
+elPool.className = "Pool: " + hotel.pool; // Update HTML with property of the object
 
-var elGym = document.getElementById("gym");
-elGym.className = "Gym: " + hotel.gym;
+var elGym = document.getElementById("gym"); // Get element
+elGym.className = "Gym: " + hotel.gym; // Update HTML with property of the object
